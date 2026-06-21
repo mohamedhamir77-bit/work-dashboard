6e0e10e093ba17cee7faf7e674212acb75556bbe
mohamedhamir77-bit <mohamed.hamir77@gmail.com>
2026-06-21 11:55:34 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,27 @@
-self.addEventListener('install', e => {
+const CACHE_NAME = "work-dashboard-v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
   );
 });
